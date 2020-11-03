d352c9faebc0538983140a3e2d2e15933b17692b
Saurabh Katkar <spiritsk9@gmail.com>
2020-11-03 02:20:18 -1200

    font updated

---
+++ app/src/main/java/com/example/saurabh/interntask/AddClass.java
@@ -0,0 +1,14 @@
+package com.example.saurabh.interntask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_class);
+    }
+}
