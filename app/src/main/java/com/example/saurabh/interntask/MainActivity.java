4f51405e1fa463dbca88cbb1728db21fecc9d65c
Saurabh Katkar <spiritsk9@gmail.com>
2020-11-02 20:06:47 -1200

    adapter in progress

---
+++ app/src/main/java/com/example/saurabh/interntask/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.saurabh.interntask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.AbsoluteSizeSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView headerMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        headerMessage = findViewById(R.id.headerMessage);
+
+        SpannableStringBuilder longDescription = new SpannableStringBuilder();
+        longDescription.append("You can ");
+        int start = longDescription.length();
+        longDescription.append("View Or Edit ");
+        longDescription.setSpan(new ForegroundColorSpan(Color.WHITE), start, longDescription.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        longDescription.setSpan(new StyleSpan(android.graphics.Typeface.BOLD), start, longDescription.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        longDescription.append("your previous class entries by clicking on them.");
+
+        headerMessage.setText(longDescription,  TextView.BufferType.SPANNABLE);
+    }
+}
