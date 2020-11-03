3c7861a9c2d85c6905e668ef53da7091e8d55852
Saurabh Katkar <spiritsk9@gmail.com>
2020-11-02 12:34:42 -1200

    reset

--- app/src/main/java/com/example/saurabh/nooratask1/AppDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.saurabh.nooratask1;
-
-import android.arch.persistence.room.Database;
-import android.arch.persistence.room.RoomDatabase;
-
-@Database(entities = {ClassData.class}, version = 1)
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract NDao nDao();
-}
